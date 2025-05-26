d057928ce6e8255b0b0184e3cb1cc750f57d332c
Yerdix7 <122042895@upq.edu.mx>
2025-05-25 23:35:44 -0600

    P3
    los 3 ejercicios de la practica 3

---
+++ Practica3/a/main.js
@@ -0,0 +1,9 @@
+import { restar } from './utils.js';
+
+const resultado1 = restar(15, 4);
+const resultado2 = restar(2, 9);
+const resultado3 = restar(0, 0);
+
+console.log("Resultado 1:", resultado1);
+console.log("Resultado 2:", resultado2);
+console.log("Resultado 3:", resultado3);
---
+++ Practica3/a/utils.js
@@ -0,0 +1,3 @@
+const restar = (a, b) => a - b;
+
+export { restar };
